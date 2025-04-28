f95cb03b269bac5bde87b77cc49de961495aa973
Ljj <2791933080@qq.com>
2025-04-28 16:39:46 +0800

    refactor(interceptor): 重构登录拦截器并添加 token 刷新功能
    - 重构 LoginInterceptor，简化为检查用户是否已登录
    - 新增 RefreshTokenInterceptor，用于刷新用户 token 有效期
    - 更新 WebConfig，添加 RefreshTokenInterceptor 并调整拦截器顺序

--- src/main/java/com/hmdp/config/WebConfig.java
+++ src/main/java/com/hmdp/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.hmdp.config;
 
 import com.hmdp.interceptor.LoginInterceptor;
+import com.hmdp.interceptor.RefreshTokenInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -14,8 +15,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Resource
     private LoginInterceptor loginInterceptor;
 
+    @Resource
+    private RefreshTokenInterceptor refreshTokenInterceptor;
+
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor)
-                .excludePathPatterns("/user/code", "/user/login", "/blog/hot", "/shop/**", "/shop-type/**", "/upload/**", "/voucher/**");
+                .excludePathPatterns("/user/code", "/user/login", "/blog/hot", "/shop/**", "/shop-type/**", "/upload/**", "/voucher/**")
+                .order(1);
+        //刷新token的拦截器
+        registry.addInterceptor(refreshTokenInterceptor)
+                .addPathPatterns("/**")
+                .order(0);
     }
 }
--- src/main/java/com/hmdp/interceptor/RefreshTokenInterceptor.java
+++ src/main/java/com/hmdp/interceptor/RefreshTokenInterceptor.java
@@ -1,42 +1,51 @@
 package com.hmdp.interceptor;
 
 
-
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.StrUtil;
 import com.hmdp.dto.UserDTO;
-import com.hmdp.entity.User;
 import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.UserHolder;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @Component
-public class LoginInterceptor implements HandlerInterceptor {
+public class RefreshTokenInterceptor implements HandlerInterceptor {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     // 在请求处理之前进行调用（Controller方法调用之前）
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //1.判断是否需要拦截(ThreadLocal中是否有用户)
-        if (UserHolder.getUser() == null){
-            //没有，需要拦截，设置状态码
-            response.setStatus(401);
-            //拦截
-            return false;
+        //1.在请求头中获取token
+        String token = request.getHeader("authorization");
+        if (StrUtil.isBlank(token)){
+            return true;
         }
-        //2.有用户，则放行
+        //2.基于token获得redis中的用户
+        String key = RedisConstants.LOGIN_USER_KEY + token;
+        Map<Object, Object> userMap = stringRedisTemplate.opsForHash().entries(key);
+        //3.判断用户是否存在
+        if(userMap.isEmpty()){
+            return true;
+        }
+
+        //5.存在，保存用户信息到ThreadLocal
+        //UserHolder.saveUser((UserDTO) user);
+        UserDTO userDTO = BeanUtil.fillBeanWithMap(userMap, new UserDTO(), false);
+        UserHolder.saveUser(userDTO);//保存用户信息到ThreadLocal
+
+        //6.刷新token有效期
+        stringRedisTemplate.expire(key, RedisConstants.LOGIN_USER_TTL, TimeUnit.MINUTES);
+
+        //7.放行
         return true;
     }
 
