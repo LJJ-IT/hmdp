f4c1d8189b9e55a2682842aef0380b852270cfc5
Ljj <2791933080@qq.com>
2025-04-28 17:04:06 +0800

    feat(shop): 添加商铺缓存功能
    - 在 IShopService接口中添加 queryById 方法
    - 修改 ShopController 中的 queryShopById 方法，使用新的 queryById 接口
    - 在 ShopServiceImpl 中实现 queryById 方法，添加缓存逻辑
    - 使用 Redis 存储商铺信息，提高查询效率

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,10 +1,17 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisConstants;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -17,4 +24,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryById(Long id) {
+        //1.从redis中查询缓存
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
+        String shopJson = stringRedisTemplate.opsForValue().get(RedisConstants.CACHE_SHOP_KEY + id);
+        //2.判断是否存在
+        if(StrUtil.isNotBlank(shopJson)){
+            //3.存在，直接返回
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return Result.ok(shop);
+        }
+        //4.不存在，查询数据库
+        Shop shop = getById(id);
+        //5.不存在返回错误
+        if(shop == null){
+            return Result.fail("商铺不存在");
+        }
+        //6.存在，写入缓存
+        stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop));
+        //7.返回数据
+        return Result.ok(shop);
+
+    }
 }
